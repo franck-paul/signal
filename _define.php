425e44486d09d25d6ef41806abc90268f9b23d5f
franck <carnet.franck.paul@gmail.com>
2021-10-23 12:44:45 +0200

    Add alternative repository

--- _define.php
+++ _define.php
@@ -10,20 +10,23 @@
  * @copyright Franck Paul carnet.franck.paul@gmail.com
  * @copyright GPL-2.0 https://www.gnu.org/licenses/gpl-2.0.html
  */
-
-if (!defined('DC_RC_PATH')) {return;}
+if (!defined('DC_RC_PATH')) {
+    return;
+}
 
 $this->registerModule(
-    "Signal",                         // Name
-    "Private comments to the author", // Description
-    "Franck Paul",                    // Author
+    'Signal',                         // Name
+    'Private comments to the author', // Description
+    'Franck Paul',                    // Author
     '0.4',                            // Version
     [
         'requires'    => [['core', '2.13']],                      // Dependencies
         'permissions' => 'admin',                                 // Permissions
         'type'        => 'plugin',                                // Type
-        'details'     => 'https://open-time.net/?q=signal',       // Details URL
-        'support'     => 'https://github.com/franck-paul/signal', // Support URL
-        'settings'    => ['blog' => '#params.signal']            // Settings
+        'settings'    => ['blog' => '#params.signal'],            // Settings
+
+        'details'    => 'https://open-time.net/?q=signal',       // Details URL
+        'support'    => 'https://github.com/franck-paul/signal', // Support URL
+        'repository' => 'https://raw.githubusercontent.com/franck-paul/signal/main/dcstore.xml'
     ]
 );
