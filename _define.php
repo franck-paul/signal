012bac4987b4c4493f2840efe9e10202e69d0656
franck <carnet.franck.paul@gmail.com>
2021-11-01 09:33:13 +0100

    Update default branch in _define.php for alternative repository

--- _define.php
+++ _define.php
@@ -27,6 +27,6 @@ $this->registerModule(
 
         'details'    => 'https://open-time.net/?q=signal',       // Details URL
         'support'    => 'https://github.com/franck-paul/signal', // Support URL
-        'repository' => 'https://raw.githubusercontent.com/franck-paul/signal/main/dcstore.xml'
+        'repository' => 'https://raw.githubusercontent.com/franck-paul/signal/master/dcstore.xml'
     ]
 );
