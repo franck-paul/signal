32c18a91c25624a2df27095004d49e78024e3bd5
franck <carnet.franck.paul@gmail.com>
2023-05-23 11:31:36 +0200

    l10n

---
+++ locales/en/main.lang.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @package Dotclear
+ *
+ * @copyright Olivier Meunier & Association Dotclear
+ * @copyright GPL-2.0-only
+ */
+#
+# DOT NOT MODIFY THIS FILE !
+#
+
+use Dotclear\Helper\L10n;
+
+L10n::$locales['Private comments to the author'] = '';
+L10n::$locales['Signal'] = '';
+L10n::$locales['Enable private comments to the author (or the moderator)'] = '';
+L10n::$locales['User defined label:'] = '';
+L10n::$locales['Leave empty to use the default one:'] = '';
+L10n::$locales['Private comment for the author (or the moderator)'] = '';
+L10n::$locales['Your private comment has been submitted.'] = '';
