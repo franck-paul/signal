80ec7b3ea228daf906752626c3fc1a3fe5bc5ba3
franck <carnet.franck.paul@gmail.com>
2023-10-24 12:49:17 +0200

    New release 4.0

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'Signal',
     'Private comments to the author',
     'Franck Paul',
-    '3.0',
+    '4.0',
     [
         'requires'    => [['core', '2.28']],
         'permissions' => 'My',
