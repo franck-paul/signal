65e16b706c538a60b403988c63b1a16b7695a2cb
franck <carnet.franck.paul@gmail.com>
2025-07-19 13:03:26 +0200

    Add Polish language (requested by Piotr Śliwka)

---
+++ locales/pl/main.lang.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package Dotclear
+ *
+ * @copyright Olivier Meunier & Association Dotclear
+ */
+#
+# DOT NOT MODIFY THIS FILE !
+#
+
+use Dotclear\Helper\L10n;
+
+L10n::$locales['Signal'] = 'Sygnał';
+L10n::$locales['Private comments to the author'] = 'Prywatne komentarze do autora';
+L10n::$locales['Enable private comments to the author (or the moderator)'] = 'Włącz prywatne komentarze do autora (lub moderatora)';
+L10n::$locales['User defined label:'] = 'Etykieta zdefiniowana przez użytkownika:';
+L10n::$locales['Leave empty to use the default one:'] = 'Pozostaw puste, aby użyć domyślnego:';
+L10n::$locales['Private comment for the author (or the moderator)'] = 'Prywatny komentarz dla autora (lub moderatora)';
+L10n::$locales['Your private comment has been submitted.'] = 'Twój prywatny komentarz został przesłany.';
