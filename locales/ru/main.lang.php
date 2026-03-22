ce5205cb3c073f91d04ff9f72dca583adce8ad24
franck <contact@open-time.net>
2026-03-22 06:01:56 +0100

    Add Russian language

---
+++ locales/ru/main.lang.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package Dotclear
+ *
+ * @copyright Olivier Meunier & Association Dotclear
+ */
+#
+# DOT NOT MODIFY THIS FILE !
+#
+
+use Dotclear\Helper\L10n;
+
+L10n::$locales['Signal'] = 'Сигнал';
+L10n::$locales['Private comments to the author'] = 'Частные комментарии автору';
+L10n::$locales['Enable private comments to the author (or the moderator)'] = 'Включить приватные комментарии для автора (или модератора)';
+L10n::$locales['User defined label:'] = 'Метка, определяемая пользователем:';
+L10n::$locales['Leave empty to use the default one:'] = 'Оставьте пустым, чтобы использовать значение по умолчанию:';
+L10n::$locales['Private comment for the author (or the moderator)'] = 'Приватный комментарий для автора (или модератора)';
+L10n::$locales['Your private comment has been submitted.'] = 'Ваш личный комментарий отправлен.';
